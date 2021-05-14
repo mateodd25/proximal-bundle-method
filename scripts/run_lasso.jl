af6899dfd87424f80871526c20f8ba221d80fd7a
Mateo Diaz <mateodd@icloud.com>
2021-05-14 16:57:23 -0400

    Update unused experiments

--- scripts/run_lasso.jl
+++ scripts/run_lasso.jl
@@ -9,78 +9,91 @@ include("utils.jl")
 include("csv_exporter.jl")
 
 function lasso_objective(x, A, b, reg_coeff)
-  err = A*x-b
-    return 1/2 * norm(err)^2 + reg_coeff * norm(x, 1)
+    err = A * x - b
+    return 1 / 2 * norm(err)^2 + reg_coeff * norm(x, 1)
 end
 
 function lasso_subgradient(x, A, b, reg_coeff)
-  err = A*x-b
-  return A'*err + reg_coeff * map(sign, x)
+    err = A * x - b
+    return A' * err + reg_coeff * map(sign, x)
 end
 
 function compute_minimum_with_cvx(A, b, reg_coeff)
-  _, n = size(A)
-  x = Convex.Variable(n)
+    _, n = size(A)
+    x = Convex.Variable(n)
 
-  problem = Convex.minimize((1/2) * Convex.sumsquares(A * x - b) + reg_coeff * Convex.norm(x, 1))
-  Convex.solve!(problem, SCS.Optimizer) #() -> SCS.Optimizer(verbose=false), verbose=false)
-  return problem
+    problem = Convex.minimize(
+        (1 / 2) * Convex.sumsquares(A * x - b) + reg_coeff * Convex.norm(x, 1),
+    )
+    Convex.solve!(problem, SCS.Optimizer) #() -> SCS.Optimizer(verbose=false), verbose=false)
+    return problem
 end
 
 function main()
-  m, n = 10, 12
-  A = randn(m, n)
-  x_opt = vec(sprandn(n, 1, .1))
+    m, n = 10, 12
+    A = randn(m, n)
+    x_opt = vec(sprandn(n, 1, 0.1))
 
-  b = A * x_opt
-  # b += 0.01 * randn(m)/sqrt(m)
-  x_init = randn(n)
-  # x_init = x_opt + 0.01 * randn(n)/sqrt(n)
-  reg_coeff = 0.05
-  objective = (x -> lasso_objective(x, A, b, reg_coeff))
-  subgradient = (x -> lasso_subgradient(x, A, b, reg_coeff))
-  step_size = (x, y, _) -> compute_ideal_step_size(x, y, objective(x_opt), x_opt)
-  # step_size = (x, _, _) -> compute_step_size(x, 0.0, objective(x_init))
+    b = A * x_opt
+    # b += 0.01 * randn(m)/sqrt(m)
+    x_init = randn(n)
+    # x_init = x_opt + 0.01 * randn(n)/sqrt(n)
+    reg_coeff = 0.05
+    objective = (x -> lasso_objective(x, A, b, reg_coeff))
+    subgradient = (x -> lasso_subgradient(x, A, b, reg_coeff))
+    step_size = (x, y, _) -> compute_ideal_step_size(x, y, objective(x_opt), x_opt)
+    # step_size = (x, _, _) -> compute_step_size(x, 0.0, objective(x_init))
 
-  println("Sloving with Convex.jl first")
-  problem = compute_minimum_with_cvx(A, b, reg_coeff)
-  Printf.@printf(
-    "Obj=%12g\n",
-    problem.optval,
-  )
+    println("Sloving with Convex.jl first")
+    problem = compute_minimum_with_cvx(A, b, reg_coeff)
+    Printf.@printf("Obj=%12g\n", problem.optval,)
 
-  println("Number of nonzeros")
-  println(nnz(x_opt))
-  println("Objective at signal")
-  println(objective(x_opt))
+    println("Number of nonzeros")
+    println(nnz(x_opt))
+    println("Objective at signal")
+    println(objective(x_opt))
 
 
-  params = create_bundle_method_parameters(
-    iteration_limit = 100000,
-    verbose = true,
-    printing_frequency = 1000,
-    full_memory = false,
-  )
-  # println("\nAbout to solve a random least squares problem using ideal step size.")
-  # ProximalBundleMethod.solve(objective, subgradient, params, step_size, x_init)
-  step_sizes = [.001 * 2^j for j in 0:19]
-  println("\nAbout to solve a random lasso problem using adaptive parallel method short memory.")
-  sol = ProximalBundleMethod.solve_adaptive(
-    objective, subgradient, params, step_sizes, x_init)
-  println(norm(sol.solution-x_opt)/norm(x_opt))
+    params = create_bundle_method_parameters(
+        iteration_limit = 100000,
+        verbose = true,
+        printing_frequency = 1000,
+        full_memory = false,
+    )
+    # println("\nAbout to solve a random least squares problem using ideal step size.")
+    # ProximalBundleMethod.solve(objective, subgradient, params, step_size, x_init)
+    step_sizes = [0.001 * 2^j for j = 0:19]
+    println(
+        "\nAbout to solve a random lasso problem using adaptive parallel method short memory.",
+    )
+    sol = ProximalBundleMethod.solve_adaptive(
+        objective,
+        subgradient,
+        params,
+        step_sizes,
+        x_init,
+    )
+    println(norm(sol.solution - x_opt) / norm(x_opt))
 
-  params = create_bundle_method_parameters(
-    iteration_limit = 1000,
-    verbose = true,
-    printing_frequency = 50,
-    full_memory = true,
-  )
-  # println("\nAbout to solve a random least squares problem using ideal step size.")
-  # ProximalBundleMethod.solve(objective, subgradient, params, step_size, x_init)
-  println("\nAbout to solve a random lasso problem using adaptive parallel method full memory.")
+    params = create_bundle_method_parameters(
+        iteration_limit = 1000,
+        verbose = true,
+        printing_frequency = 50,
+        full_memory = true,
+    )
+    # println("\nAbout to solve a random least squares problem using ideal step size.")
+    # ProximalBundleMethod.solve(objective, subgradient, params, step_size, x_init)
+    println(
+        "\nAbout to solve a random lasso problem using adaptive parallel method full memory.",
+    )
     sol, iter_info = ProximalBundleMethod.solve_adaptive(
-      objective, subgradient, params, step_sizes, x_init)
-  println(norm(sol.solution-x_opt)/norm(x_opt))
+        objective,
+        subgradient,
+        params,
+        step_sizes,
+        x_init,
+    )
+    println(norm(sol.solution - x_opt) / norm(x_opt))
 
 end
 
--- scripts/run_maximum_quadratics.jl
+++ scripts/run_maximum_quadratics.jl
@@ -5,99 +5,113 @@ import SCS
 import Printf
 
 include("utils.jl")
+include("csv_exporter.jl")
+include("plot_utils.jl")
 
 struct Quadratic
-  constant::Float64
-  slope::Vector{Float64}
-  matrix::Matrix{Float64}
+    constant::Float64
+    slope::Vector{Float64}
+    matrix::Matrix{Float64}
 end
 
 function evaluate_quadratic(x::Vector{Float64}, quadratic::Quadratic)
-  return 1 / 2 * LinearAlgebra.norm(quadratic.matrix * x)^2 + quadratic.slope' * x + quadratic.constant
+    return 1 / 2 * LinearAlgebra.norm(quadratic.matrix * x)^2 +
+           quadratic.slope' * x +
+           quadratic.constant
 end
 
 function evaluate_gradient_quadratic(x::Vector{Float64}, quadratic::Quadratic)
-  return quadratic.matrix * (quadratic.matrix * x) + quadratic.slope
+    return quadratic.matrix * (quadratic.matrix * x) + quadratic.slope
 end
 
 function maximum_quadratic_objective(x, quadratics::Vector{Quadratic})
-  return maximum([evaluate_quadratic(x, quadratic) for quadratic in quadratics])
+    return maximum([evaluate_quadratic(x, quadratic) for quadratic in quadratics])
 end
 
 function maximum_quadratic_gradient(x, quadratics)
-  _, idx = findmax([evaluate_quadratic(x, quadratic) for quadratic in quadratics])
-  if  sum([(quadratic == quadratics[idx] ? 1.0 : 0.0) for quadratic in quadratics]) > 1.0
-    println("Here")
-  end
-  grad = evaluate_gradient_quadratic(x, quadratics[idx])
-  # println(LinearAlgebra.norm(grad))
-  return grad
+    _, idx = findmax([evaluate_quadratic(x, quadratic) for quadratic in quadratics])
+    if sum([(quadratic == quadratics[idx] ? 1.0 : 0.0) for quadratic in quadratics]) > 1.0
+        println("Here")
+    end
+    grad = evaluate_gradient_quadratic(x, quadratics[idx])
+    # println(LinearAlgebra.norm(grad))
+    return grad
 end
 
 function compute_minimum_with_cvx(quadratics::Vector{Quadratic})
-  x = Convex.Variable(length(quadratics[1].slope))
-  t = Convex.Variable(1)
+    x = Convex.Variable(length(quadratics[1].slope))
+    t = Convex.Variable(1)
 
-  problem = Convex.minimize(t)
-  for quadratic in quadratics
-    problem.constraints += t >= 1 / 2 * Convex.sumsquares(quadratic.matrix * x) + quadratic.slope' * x + quadratic.constant
-  end
-  Convex.solve!(problem, SCS.Optimizer) #() -> SCS.Optimizer(verbose=false), verbose=false)
-  return problem
+    problem = Convex.minimize(t)
+    for quadratic in quadratics
+        problem.constraints +=
+            t >=
+            1 / 2 * Convex.sumsquares(quadratic.matrix * x) +
+            quadratic.slope' * x +
+            quadratic.constant
+    end
+    Convex.solve!(problem, SCS.Optimizer) #() -> SCS.Optimizer(verbose=false), verbose=false)
+    return problem
 end
 
 function main()
-  d = 100
-  k = 10
+    d = 20
+    k = 3
 
-  quadratics = Quadratic[]
-  for i in 1:k
-    Q = randn(d, d)/sqrt(d)
-    Q = Q' * Q
-    push!(quadratics, Quadratic(randn(), randn(d), Q))
-  end
+    quadratics = Quadratic[]
+    for i = 1:k
+        Q = randn(d, d) / sqrt(d)
+        Q = Q' * Q
+        # Q = zeros(d, d)
+        push!(quadratics, Quadratic(randn(), randn(d), Q))
+    end
 
-  # println("Solving with Convex.jl first")
-  # problem = compute_minimum_with_cvx(quadratics)
-  # Printf.@printf(
-  #   "Obj=%12g\n",
-  #   problem.optval,
-  # )
+    println("Solving with Convex.jl first")
+    problem = compute_minimum_with_cvx(quadratics)
+    Printf.@printf("Obj=%12g\n", problem.optval,)
 
-  x_init = randn(d)
+    x_init = randn(d)
 
-  objective = (x -> maximum_quadratic_objective(x, quadratics))
-  gradient = (x -> maximum_quadratic_gradient(x, quadratics))
-  # step_size = (x, y, _) -> compute_ideal_step_size(x, y, 0.0, x_opt)
-  # step_size = (x, _, _) -> compute_step_size(x, 0.0, objective(x_init))
+    objective = (x -> maximum_quadratic_objective(x, quadratics) - problem.optval)
+    gradient = (x -> maximum_quadratic_gradient(x, quadratics))
+    # step_size = (x, y, _) -> compute_ideal_step_size(x, y, 0.0, x_opt)
+    # step_size = (x, _, _) -> compute_step_size(x, 0.0, objective(x_init))
 
-  # params = create_bundle_method_parameters(
-  # iteration_limit = 100000,
-  # verbose = true,
-  # printing_frequency = 1000,
-  # )
+    # params = create_bundle_method_parameters(
+    # iteration_limit = 100000,
+    # verbose = true,
+    # printing_frequency = 1000,
+    # )
 
-  step_sizes = [.000001 * 2^j for j in 0:15]
-  params = create_bundle_method_parameters(
-    iteration_limit = 3000,
-    verbose = true,
-    printing_frequency = 100,
-    # full_memory = true,
-  )
-  # println("About to solve a random least squares problem using ideal step size.")
-  # ProximalBundleMethod.solve(objective, gradient, params, step_size, x_init)
-  println("\nAbout to solve a random maximum of quadratics using adaptive parallel method with short memory.")
-  ProximalBundleMethod.solve_adaptive(
-    objective, gradient, params, step_sizes, x_init)
-  #     params = create_bundle_method_parameters(
-  #         iteration_limit = 1000,
-  #         verbose = true,
-  #         printing_frequency = 100,
-  #         full_memory = true,
-  #     )
-  #     println("\nAbout to solve a random maximum of quadratics using adaptive parallel method with full memory.")
-  #     ProximalBundleMethod.solve_adaptive(
-  #         objective, gradient, params, ProximalBundleMethod.AdaptiveStepSizeInterval(.000001, 16), x_init)
+    step_sizes = [1e-20 * 10^j for j = 0:22]
+    params = create_bundle_method_parameters(
+        iteration_limit = 20000,
+        verbose = true,
+        printing_frequency = 100,
+        # full_memory = true,
+    )
+    # println("About to solve a random least squares problem using ideal step size.")
+    # ProximalBundleMethod.solve(objective, gradient, params, step_size, x_init)
+    println(
+        "\nAbout to solve a random maximum of quadratics using adaptive parallel method with short memory.",
+    )
+    sol_ideal, _ =
+        ProximalBundleMethod.solve_adaptive(objective, gradient, params, step_sizes, x_init)
+
+    csv_path_ideal = "/tmp/rusults_ideal_step_size_max_quad.csv"
+    output_stepsize_idea_plot = "/tmp/rusults_ideal_step_size_max_quad.pdf"
+    export_statistics(sol_ideal, csv_path_ideal)
+    plot_objective(csv_path_ideal, output_stepsize_idea_plot)
+
+    #     params = create_bundle_method_parameters(
+    #         iteration_limit = 1000,
+    #         verbose = true,
+    #         printing_frequency = 100,
+    #         full_memory = true,
+    #     )
+    #     println("\nAbout to solve a random maximum of quadratics using adaptive parallel method with full memory.")
+    #     ProximalBundleMethod.solve_adaptive(
+    #         objective, gradient, params, ProximalBundleMethod.AdaptiveStepSizeInterval(.000001, 16), x_init)
 end
 
 main()
